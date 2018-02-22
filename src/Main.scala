f35398172e560a035d69b0748db0f842482e2ae3
Robert Kleemann <kleemann@scharp.org>
2018-02-22 13:14:49 -0800

    cleand up imports

--- src/Main.scala
+++ src/Main.scala
@@ -5,8 +5,8 @@
  * https://mypuzzles.wordpress.com/solution-the-snake-cube/
  */
 object Main extends App {
-  import org.kleemann.snakepuzzle._
-  import org.kleemann.snakepuzzle.Block._
+  import org.kleemann.snakepuzzle.{Block,Solution,solve,prune}
+  import org.kleemann.snakepuzzle.Block.{Straight,RightAngle}
 
   /**
    * The structure of an unpositioned snake puzzle
