a65f652e7ac16e95ea2043fe855d5188005a7bf9
Robert Kleemann <robert@kleemann.org>
2018-02-22 17:30:57 -0800

    improved comments

--- src/org/kleemann/snakepuzzle/CubeExtent.scala
+++ src/org/kleemann/snakepuzzle/CubeExtent.scala
@@ -4,9 +4,13 @@ package org.kleemann.snakepuzzle {
   /**
    * Keeps track of the size the bounding cube is getting as new block
    * placements are made.  A successfully solved puzzle must have all
-   * 27 placed cubes fit within a 3x3 bounding cube. If the bounding
-   * cube of placed cubes is ever greater than 3x3 then the collective
+   * placed cubes fit within a bounding cube. E.g. a snake of length 27
+   * must fit in a 3x3 bounding cube. If the bounding cube of placed
+   * cubes is ever greater than the extent then the collective
    * placements are not (and can never be) a valid solution.
+   *
+   * Due to the private constructor, a CubeExtent object can never exceed
+   * the given extent.
    *
    * Each axis value in min is guaranteed to be less than or equal to
    * the corresponding axis value in max.
@@ -17,7 +21,9 @@ package org.kleemann.snakepuzzle {
   case class CubeExtent private (maxExtent: Int, min: Coordinate, max: Coordinate) {
 
     /**
-     * Add the given coordination to the extent, expanding the extent if necessary
+     * Add the given coordination to the extent, expanding the extent if necessary.
+     * Returns None if the given coordinated causes the CubeExtent to exceed the
+     * maxExtent.
      *
      * I'm drinking the Scala punctuation kool-aide and using operators instead of
      * named functions.
