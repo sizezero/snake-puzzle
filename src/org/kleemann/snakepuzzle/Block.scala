ed7e1dc938e481068a61e5fc49226598da93470b
Robert Kleemann <kleemann@scharp.org>
2018-03-01 08:44:14 -0800

    style

--- src/org/kleemann/snakepuzzle/Block.scala
+++ src/org/kleemann/snakepuzzle/Block.scala
@@ -5,8 +5,7 @@ package org.kleemann.snakepuzzle {
    * A single block in the link's chain.
    * This is the structure of an unplaced block.
    */
-  sealed trait Block {
-  }
+  sealed trait Block
 
   object Block {
     object Straight extends Block {
